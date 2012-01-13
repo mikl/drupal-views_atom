5c5d01774cadcc7b64f671b83504d9aaa4c4f097
Steve Persch <persch@palantir.net>
2012-01-13 15:42:23 -0600

    refs #7792, updating tombstone handler.

--- views-atom-tombstone.tpl.php
+++ views-atom-tombstone.tpl.php
@@ -2,16 +2,14 @@
 
 <feed xmlns="http://www.w3.org/2005/Atom" xmlns:at="http://purl.org/atompub/tombstones/1.0">
   <title><?php print $title; ?></title>
-  <link href="<?php print $link ?>"/>
+  <link href="<?php print $feed_url ?>" />
   <?php if ($use_push) : ?>
     <link rel="hub" href="<?php echo $hub_url; ?>" />
-    <link rel="self" href="<?php echo $feed_url; ?>" />
   <?php endif; ?>
   <updated><?php echo $updated; ?></updated>
-  <generator uri="<?php echo $link; ?>">Drupal</generator>
+  <generator uri="<?php echo $feed_url; ?>">Drupal</generator>
   <?php foreach ($items as $item) : ?>
-    <at:deleted-entry ref="<?php echo $item['guid']; ?>" when="<?php echo $item['when']; ?>">
-      <?php if ($item['by']) : ?>
+    <at:deleted-entry ref="<?php echo $item['guid']; ?>" when="<?php echo $item['when']; ?>"<?php if (empty($item['by'])) { ?>/><?php } else { ?>>
           <at:by>
             <?php if ($item['by']['name']) :?>
               <name><?php echo $item['by']['name']; ?></name>
@@ -20,10 +18,10 @@
               <email><?php echo $item['by']['email']; ?></email>
             <?php endif; ?>
           </at:by>
-      <?php endif; ?>
       <?php if (!empty($item['comment'])) : ?>
         <at:comment><?php echo $item['comment']; ?></at:comment>
       <?php endif; ?>
     </at:deleted-entry>
+    <?php } ?>
   <?php endforeach; ?>
 </feed>
