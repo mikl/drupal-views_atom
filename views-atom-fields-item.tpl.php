4f1fd895e90d9d6d965bbdc463f212e3c368dd5b
Mikkel Hoegh <mikkel@hoegh.org>
2012-05-17 19:31:28 +0200

    Only print author-tag if non-empty.

--- views-atom-fields-item.tpl.php
+++ views-atom-fields-item.tpl.php
@@ -6,11 +6,13 @@
   <id><?php echo $atom_uuid; ?></id>
   <updated><?php echo $atom_updated; ?></updated>
   <published><?php echo $atom_published; ?></published>
+  <?php if (!empty($atom_author) || !empty($atom_author_email) || !empty($atom_author_url)): ?>
   <author>
     <name><?php print $atom_author; ?></name>
     <email><?php print $atom_author_email; ?></email>
     <uri><?php print $atom_author_url; ?></uri>
   </author>
+  <?php endif; ?>
   <content type="<?php echo $entry_type; ?>" xml:lang="<?php echo $language; ?>">
     <?php print $content; ?>
   </content>
